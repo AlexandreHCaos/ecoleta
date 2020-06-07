09cc6bf2ed5773779778fa206237757f30925897
Alexandre H Caos <alexandrecaos@hotmail.com>
2020-06-07 13:58:56 -0300

    Celebrate e Drag 'n' Drop Image

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -16,6 +16,7 @@ interface Params {
 interface Data {
   point: {
     image: string
+    image_url: string
     name: string
     email: string
     whatsapp: string
@@ -57,6 +58,8 @@ const Points = () => {
   }
 
   if (!data.point) {
+    console.log(data);
+    
     return null
   }
 
@@ -67,7 +70,7 @@ const Points = () => {
           <Icon name="arrow-left" color="#34cb78" size={20} />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>
           {data.items.map(item => item.title).join(', ')}
